efb82e68f1b3c785f4772537c0f18fb41aacff0d
Clement HOUBRON <71270034+ClementHoubron@users.noreply.github.com>
2025-03-11 15:42:19 +0100

    feat(serveur): add serveur for transactions

---
+++ Projet.Serveur.Data/Repositories/TransactionRepository.cs
@@ -0,0 +1,37 @@
+﻿using Projet.Serveur.Data.Entities;
+
+namespace Projet.Serveur.Data.Repositories
+{
+    public interface ITransactionRepository
+    {
+        void AddTransaction(Transaction transaction);
+        IEnumerable<Transaction> GetValidTransactions();
+        IEnumerable<Transaction> GetAnomalies();
+    }
+
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly MyDbContext _context;
+
+        public TransactionRepository(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddTransaction(Transaction transaction)
+        {
+            _context.Transactions.Add(transaction);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Transaction> GetValidTransactions()
+        {
+            return _context.Transactions.Where(t => t.EstValide).ToList();
+        }
+
+        public IEnumerable<Transaction> GetAnomalies()
+        {
+            return _context.Transactions.Where(t => !t.EstValide).ToList();
+        }
+    }
+}
